0061c501e0bbf5322619fa63f7359c367738fc30
Simen Brekken <simen@unfold.no>
2017-04-06 07:27:07 +0200

    Simplify superagent adapater

--- src/adapters/superagent.js
+++ src/adapters/superagent.js
@@ -1,25 +1,7 @@
 import superagent from 'superagent';
-import * as httpMethods from '../constants/http-methods';
-
-export const createRequest = (url, method) => {
-    switch (method) {
-        case httpMethods.GET:
-            return superagent.get(url);
-        case httpMethods.POST:
-            return superagent.post(url);
-        case httpMethods.PUT:
-            return superagent.put(url);
-        case httpMethods.PATCH:
-            return superagent.patch(url);
-        case httpMethods.DELETE:
-            return superagent.del(url);
-        default:
-            throw new Error(`Unsupported HTTP method: ${method}`);
-    }
-};
 
 const superagentNetworkAdapter = (url, method, { body, headers, credentials } = {}) => {
-    const request = createRequest(url, method);
+    const request = superagent(method, url);
 
     if (body) {
         request.send(body);
