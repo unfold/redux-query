f207c53a74c30e24edba677f086c9991bd3e3c48
Simen Brekken <simen@unfold.no>
2017-04-06 07:35:07 +0200

    Update tests

--- test/adapters/superagent.test.js
+++ test/adapters/superagent.test.js
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
 import * as HTTPMethods from '../../src/constants/http-methods';
-import superagentAdapter, { createRequest } from '../../src/adapters/superagent';
+import superagentAdapter from '../../src/adapters/superagent';
 
 describe('superagent adapter', () => {
     it('must return an object with both execute and abort functions', () => {
@@ -11,33 +11,28 @@ describe('superagent adapter', () => {
 
     describe('createRequest', () => {
         it('must return a DELETE request when supplied a DELETE method', () => {
-            const request = createRequest('http://localhost', HTTPMethods.DELETE);
-            assert.equal(request.method, HTTPMethods.DELETE);
+            const adapter = superagentAdapter('http://localhost', HTTPMethods.DELETE);
+            assert.equal(adapter.instance.method, HTTPMethods.DELETE);
         });
 
         it('must return a GET request when supplied a GET method', () => {
-            const request = createRequest('http://localhost', HTTPMethods.GET);
-            assert.equal(request.method, HTTPMethods.GET);
+            const adapter = superagentAdapter('http://localhost', HTTPMethods.GET);
+            assert.equal(adapter.instance.method, HTTPMethods.GET);
         });
 
         it('must return a PATCH request when supplied a PATCH method', () => {
-            const request = createRequest('http://localhost', HTTPMethods.PATCH);
-            assert.equal(request.method, HTTPMethods.PATCH);
+            const adapter = superagentAdapter('http://localhost', HTTPMethods.PATCH);
+            assert.equal(adapter.instance.method, HTTPMethods.PATCH);
         });
 
         it('must return a POST request when supplied a POST method', () => {
-            const request = createRequest('http://localhost', HTTPMethods.POST);
-            assert.equal(request.method, HTTPMethods.POST);
+            const adapter = superagentAdapter('http://localhost', HTTPMethods.POST);
+            assert.equal(adapter.instance.method, HTTPMethods.POST);
         });
 
         it('must return a PUT request when supplied a PUT method', () => {
-            const request = createRequest('http://localhost', HTTPMethods.PUT);
-            assert.equal(request.method, HTTPMethods.PUT);
-        });
-
-        it('must throw an error when supplied an invalid HTTP method', () => {
-            const request = () => createRequest('http://localhost', 'abc');
-            assert.throws(request, /Unsupported HTTP method/);
+            const adapter = superagentAdapter('http://localhost', HTTPMethods.PUT);
+            assert.equal(adapter.instance.method, HTTPMethods.PUT);
         });
     });
 });
